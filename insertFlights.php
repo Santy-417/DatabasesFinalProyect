0fc247e6760df1e94abe223e73282223845c0b7c
Santy-417 <santichav417@gmail.com>
2024-05-21 02:04:07 -0500

    first change of flights

--- insertFlights.php
+++ insertFlights.php
@@ -3,12 +3,16 @@ include('conexion_bd.php');
 //ESTA SENTENCIA ESTABLECE UNA CONEXIÓN CON LA BASE DE DATOS
 $conexion = conexion_bd();
 
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$puesto = $_POST['puesto'];
+$numero_vuelo = $_POST['numero_vuelo'];
+$aerolinea = $_POST['aerolinea'];
+$origen = $_POST['origen'];
+$origen = $_POST['departamento'];
+$departamento = $_POST['departamento'];
+$departamento = $_POST['departamento'];
+$departamento = $_POST['departamento'];
 $departamento = $_POST['departamento'];
 //ESTA INSTURCCION CONSTRUYE EL INSERT PARA LA TABLA CLIENTES
-$sql = "INSERT INTO personal (nombre, apellido, puesto, departamento) VALUES ('$nombre', '$apellido', '$puesto', '$departamento')";
+$sql = "INSERT INTO vuelos (numero_vuelo, aerolinea, origen, departamento) VALUES ('$numero_vuelo', '$aerolinea', '$origen', '$departamento')";
 //echo $sql;
 //EJECUTA EL SQL EN LA BASE DE DATOS ESTABLECIDA EN LA CONEXIÓN
 $consulta = mysqli_query($conexion, $sql);
